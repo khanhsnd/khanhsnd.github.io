3b156d4f63fc0ee27addc4690e4b14a774dbd873
khanhnd <duckhanh.0811@gmail.com>
2020-03-11 15:20:50 +0700

    update

--- js/app.js
+++ js/app.js
@@ -6,86 +6,50 @@
 
 !function ($) {
     "use strict";
-    var MainApp = function () {};
-   
-        //===== ripples =====
+    var MainApp = function () { };
 
-        MainApp.prototype.initprofileRipple = function () {
+    //===== ripples =====
 
-           $('#profile_ripple').ripples({
-                resolution: 512,
-                dropRadius: 20, //px
-                perturbance: 0.04,
-            });            
-        },
+    MainApp.prototype.initprofileRipple = function () {
 
-        //=====counter =====
-       
-        MainApp.prototype.initCounter = function () {
-            var a = 0;
-            $(window).scroll(function() {
-                var oTop = $('#counter').offset().top - window.innerHeight;
-                if (a == 0 && $(window).scrollTop() > oTop) {
-                    $('.counter-value').each(function() {
-                        var $this = $(this),
-                            countTo = $this.attr('data-count');
-                        $({
-                            countNum: $this.text()
-                        }).animate({
-                                countNum: countTo
-                            },
-
-                            {
-                                duration: 2000,
-                                easing: 'swing',
-                                step: function() {
-                                    $this.text(Math.floor(this.countNum));
-                                },
-                                complete: function() {
-                                    $this.text(this.countNum);
-                                    //alert('finished');
-                                }
-
-                            });
-                    });
-                    a = 1;
-                }
-            });
-        },
-
+        $('#profile_ripple').ripples({
+            resolution: 512,
+            dropRadius: 20, //px
+            perturbance: 0.04,
+        });
+    },
         //=====Filter=====
-
         MainApp.prototype.initFilter = function () {
-            $(window).on('load', function() {
-            //PORTFOLIO FILTER 
-            var $container = $('.projects-wrapper');
-            var $filter = $('#filter');
-            // Initialize isotope 
-            $container.isotope({
-                filter: '*',
-                layoutMode: 'masonry',
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear'
-                }
-            });
-            // Filter items when filter link is clicked
-            $filter.find('a').click(function() {
-                var selector = $(this).attr('data-filter');
-                $filter.find('a').removeClass('active');
-                $(this).addClass('active');
+            $(window).on('load', function () {
+                //PORTFOLIO FILTER 
+                var $container = $('.projects-wrapper');
+                var $filter = $('#filter');
+                // Initialize isotope 
                 $container.isotope({
-                    filter: selector,
+                    filter: '*',
+                    layoutMode: 'masonry',
                     animationOptions: {
-                        animationDuration: 750,
-                        easing: 'linear',
-                        queue: false,
+                        duration: 750,
+                        easing: 'linear'
                     }
                 });
-                return false;
+                // Filter items when filter link is clicked
+                $filter.find('a').click(function () {
+                    var selector = $(this).attr('data-filter');
+                    $filter.find('a').removeClass('active');
+                    $(this).addClass('active');
+                    $container.isotope({
+                        filter: selector,
+                        animationOptions: {
+                            animationDuration: 750,
+                            easing: 'linear',
+                            queue: false,
+                        }
+                    });
+                    return false;
+                });
+                /*END*/
             });
-            /*END*/
-          });
         },
 
         //===== magnific Popup =====
@@ -99,26 +63,26 @@
                     enabled: true,
                     navigateByImgClick: true,
                     preload: [0, 1]
-                        // Will preload 0 - before current, and 1 after the current image 
+                    // Will preload 0 - before current, and 1 after the current image 
                 }
             });
         },
 
-         //===== magnific Popup =====
+        //===== magnific Popup =====
         MainApp.prototype.initPrint = function () {
-            $('#lnkPrint').click(function(){
-                
-                 window.print();
+            $('#lnkPrint').click(function () {
+
+                window.print();
             });
         },
 
         //===== contact =====
         MainApp.prototype.initContact = function () {
-            $('#contact-form').submit(function() {
+            $('#contact-form').submit(function () {
 
                 var action = $(this).attr('action');
 
-                $("#message").slideUp(750, function() {
+                $("#message").slideUp(750, function () {
                     $('#message').hide();
 
                     $('#submit')
@@ -126,14 +90,14 @@
                         .attr('disabled', 'disabled');
 
                     $.post(action, {
-                            name: $('#name').val(),
-                            email: $('#email').val(),
-                            comments: $('#comments').val(),
-                        },
-                        function(data) {
+                        name: $('#name').val(),
+                        email: $('#email').val(),
+                        comments: $('#comments').val(),
+                    },
+                        function (data) {
                             document.getElementById('message').innerHTML = data;
                             $('#message').slideDown('slow');
-                            $('#cform img.contact-loader').fadeOut('slow', function() {
+                            $('#cform img.contact-loader').fadeOut('slow', function () {
                                 $(this).remove()
                             });
                             $('#submit').removeAttr('disabled');
@@ -144,10 +108,9 @@
                 return false;
             });
         },
-        
+
         MainApp.prototype.init = function () {
             this.initprofileRipple();
-            this.initCounter();
             this.initFilter();
             this.initmagnificPopup();
             this.initPrint();
@@ -157,7 +120,7 @@
         $.MainApp = new MainApp, $.MainApp.Constructor = MainApp
 }(window.jQuery),
 
-//initializing
+    //initializing
     function ($) {
         "use strict";
         $.MainApp.init();
